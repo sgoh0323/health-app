b22dc09e8fc5a98debed551a9dcc9eec2334b53e
DESKTOP-9KD95OG\O <sgoh0323@gmail.com>
2022-05-18 10:14:46 +0900

    Updates

--- config-overrides.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-// const { override, fixBabelImports, setWebpackPublicPath, addWebpackPlugin } = require('customize-cra');
-
-// module.exports = override(
-//     fixBabelImports('import', {
-//         libraryName: 'antd-mobile',
-//         style: 'css'
-//     })
-// );
--- src/Popups/index.tsx
+++
@@ -1,5 +0,0 @@
-import Manager from './Manager';
-import ReplyPopup from './Reply';
-import Answer from './Answer';
-
-export { Manager, ReplyPopup, Answer };
