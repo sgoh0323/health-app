fea004578a160329203d2c9e3f69c23f23094545
DESKTOP-9KD95OG\O <sgoh0323@gmail.com>
2022-05-18 13:43:14 +0900

    Updates

--- src/layout/footer.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Footer: React.FunctionComponent = () => {
-    return <footer className="copyRight"> Copyright © 2000-2100 aimmed. All rights reserved. </footer>;
-};
-
-export default Footer;
